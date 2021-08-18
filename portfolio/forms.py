8caa224531e63ad03dbeceab5053210dfeba10ba
siphiwe <thapiemlambo@gmail.com>
2021-08-19 00:43:21 +0200

    Validate latitude and longitude values

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,12 +1,29 @@
 from django import forms
 from .models import (Profile, User)
-from django_google_maps import widgets as map_widgets
-from django_google_maps import fields as map_fields
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('cellphone', 'house_no', 'street', 'surburb', 'city', 'state', 'latitude', 'longitude')
+
+    def clean_latitude(self):
+        latitude = self.cleaned_data.get('latitude')
+
+        try:
+            float(latitude)
+            return latitude
+        except ValueError:
+            raise forms.ValidationError("Field only expects decimal values.")
+
+
+    def clean_longitude(self):
+        longitude = self.cleaned_data.get('longitude')
+
+        try:
+            float(longitude)
+            return longitude
+        except ValueError:
+            raise forms.ValidationError("Field only expects decimal values.")
 
 
 class UserForm(forms.ModelForm):
@@ -18,4 +35,3 @@ class UserForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100)
     password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
-
