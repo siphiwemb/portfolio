2691a9a93dd1c00660753057e39a2d26c4545182
siphiwe <thapiemlambo@gmail.com>
2021-08-20 12:20:06 +0200

    Undo debug setting

--- my_django_portfolio/settings.py
+++ my_django_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '18^fni3em(aqs84-^@&jjm9ydvq03@*i@bp&2ypxd$g0)pov0)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost"]
 
